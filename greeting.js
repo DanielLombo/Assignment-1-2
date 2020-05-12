7f70f094eb7705e01fc60a1791d8f79782dba894
DanielLombo <64979810+DanielLombo@users.noreply.github.com>
2020-05-12 13:56:04 +0800

    Commit

---
+++ greeting.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text } from 'react-native';
+const Greeting = props => {
+ return <Text>{props.text}</Text>
+}
+export default Greeting;
